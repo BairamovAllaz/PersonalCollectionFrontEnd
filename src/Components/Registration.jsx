dcd26719bf45c5419051f86454b8e3dcd90d90de
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-04 22:18:04 +0300

    Init

---
+++ src/Components/Registration.jsx
@@ -0,0 +1,9 @@
+import './Registration.scss'
+function Registration() {
+    return (
+        <div className="App">
+            <div className="d"></div>
+        </div>
+    );
+}
+export default Registration;
