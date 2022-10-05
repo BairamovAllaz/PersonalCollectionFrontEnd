490e430a7ece82d123fe708cee9ac4539e41f80d
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-06 00:20:02 +0300

    Fixed Page Not Found

--- src/Components/Auth/Registration.jsx
+++ src/Components/Auth/Registration.jsx
@@ -5,9 +5,54 @@ import Button from '@mui/material/Button';
 import {SiPrivateinternetaccess} from 'react-icons/si'
 import {BiImageAdd} from 'react-icons/bi'
 import GoogleButton from 'react-google-button'
+import axios from 'axios';
 
 function Registration() {
-    const [isLog, setisLog] = useState(false);
+    const [isLog, setisLog] = useState(true)
+    const [firstName,setfirstName] = useState("");
+    const [lastName,setlastName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [image,setImage] = useState("");
+
+    const LoginClick = (e) => {
+        e.preventDefault();
+        const UserData = new FormData();
+        UserData.append("email",email);
+        UserData.append("password",password);
+        axios.post("http://localhost:5100/v1/login",UserData).then(() => {
+            console.log("login succes");
+        }).catch((err) => {
+            alert(err.response.data)
+        })
+        clearInputs();
+    }
+
+    const RegisterClick = (e) => {
+         e.preventDefault();
+         const UserData = new FormData();
+         UserData.append("firstName",firstName);
+         UserData.append("lastName",lastName);
+         UserData.append("email",email);
+         UserData.append("password",password);
+         UserData.append("image",image);
+         axios.post("http://localhost:5100/v1/register",UserData).then(() => {
+             console.log("Registering succes");
+         }).catch((err) => {
+             alert(err.response.data)
+         })
+        clearInputs();
+    }
+
+
+    function clearInputs() {
+        setfirstName("");
+        setlastName("");
+        setEmail("");
+        setPassword("");
+    }
+
+
     return (
         <div className="RegisterMain">
             <div className="Main">
@@ -19,7 +64,11 @@ function Registration() {
                             height: "100%",
                             background: `${isLog ? "linear-gradient(to bottom,#cb42f5,#f542ec)" : "#f0f3f7"}`,
                             color: `${isLog == true ? "white" : "black"}`
-                        }} onClick={() => setisLog(true)}
+                        }} onClick={() => {
+                            setisLog(true)
+                            setfirstName("");
+                            setlastName("")
+                        }}
                         >Login</Button>
                     </div>
                     <div className="SignUpToggle">
@@ -28,7 +77,11 @@ function Registration() {
                             height: "100%",
                             background: `${!isLog ? "linear-gradient(to bottom,#cb42f5,#f542ec)" : "#f0f3f7"}`,
                             color: `${!isLog ? "white" : "black"}`
-                        }} onClick={() => setisLog(false)}
+                        }} onClick={() => {
+                            setisLog(false)
+                            setfirstName("");
+                            setlastName("")
+                        }}
                         >Sign</Button>
                     </div>
                 </div>
@@ -37,27 +90,46 @@ function Registration() {
                         isLog ? (
                             <div>
                                 <TextField className="outlined-basic" label="Email*" variant="outlined"
-                                           style={{marginTop: "20px"}}/>
+                                           style={{marginTop: "20px"}}
+                                           onChange={(e) => setEmail(e.target.value)}
+                                           value = {email}
+                                />
                                 <TextField className="outlined-basic" label="Password*" variant="outlined" type="password"
-                                           style={{marginTop: "20px"}}/>
+                                           style={{marginTop: "20px"}}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                           value = {password}
+                                />
                                 <Button variant="contained" style={{
                                     background: "linear-gradient(to bottom,#cb42f5,#f542ec)",
                                     marginTop: "20px",
                                     width: "70%",
                                     height: "50px"
                                 }}
+                                        onClick={LoginClick}
                                 >Login</Button>
                             </div>
                         ) : (
                             <div>
                                 <TextField className="outlined-basic" label="FirstName*" variant="outlined"
-                                           style={{marginTop: "20px"}}/>
+                                           style={{marginTop: "20px"}}
+                                            onChange = {(e) => setfirstName(e.target.value)}
+                                           value = {firstName}
+                                />
                                 <TextField className="outlined-basic" label="LastName*" variant="outlined"
-                                           style={{marginTop: "20px"}}/>
+                                           style={{marginTop: "20px"}}
+                                            onChange={(e) => setlastName(e.target.value)}
+                                           value = {lastName}
+                                />
                                 <TextField className="outlined-basic" label="Email*" variant="outlined"
-                                           style={{marginTop: "20px"}}/>
+                                           style={{marginTop: "20px"}}
+                                            onChange = {(e) => setEmail(e.target.value)}
+                                           value = {email}
+                                />
                                 <TextField className="outlined-basic" label="Password*" variant="outlined" type="password"
-                                           style={{marginTop: "20px"}}/>
+                                           style={{marginTop: "20px"}}
+                                            onChange = {(e) => setPassword(e.target.value)}
+                                           value = {password}
+                                />
                                 <Button
                                     variant="contained"
                                     component="label"
@@ -77,6 +149,10 @@ function Registration() {
                                     <input
                                         type="file"
                                         hidden
+                                        onChange={(e) => {
+                                            setImage(e.target.files[0])
+                                            console.log(e.target.files[0]);
+                                        }}
                                     />
                                 </Button>
                                 <Button variant="contained" style={{
@@ -85,6 +161,7 @@ function Registration() {
                                     width: "70%",
                                     height: "50px"
                                 }}
+                                        onClick={RegisterClick}
                                 >Sign</Button>
                             </div>
                         )
