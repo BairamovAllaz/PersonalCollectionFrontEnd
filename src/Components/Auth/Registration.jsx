9d772525403d441b5f585b4e6c072b195c22329a
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-05 20:23:14 +0300

    Fixed Responsive and Added Input (image) button

--- src/Components/Auth/Registration.jsx
+++ src/Components/Auth/Registration.jsx
@@ -3,15 +3,14 @@ import {useState} from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {SiPrivateinternetaccess} from 'react-icons/si'
+import {BiImageAdd} from 'react-icons/bi'
 import GoogleButton from 'react-google-button'
 
 function Registration() {
     const [isLog, setisLog] = useState(false);
     return (
         <div className="RegisterMain">
-            <div className="Main" style={{
-                height: `${isLog ? "540px" : "780px"}`
-            }}>
+            <div className="Main">
                 <h3 className="header">{isLog ? "Login" : "Sign"}</h3>
                 <div className="ToggleContainer">
                     <div className="LoginToggle">
@@ -28,7 +27,7 @@ function Registration() {
                             width: "100%",
                             height: "100%",
                             background: `${!isLog ? "linear-gradient(to bottom,#cb42f5,#f542ec)" : "#f0f3f7"}`,
-                            color: `${isLog == false ? "white" : "black"}`
+                            color: `${!isLog ? "white" : "black"}`
                         }} onClick={() => setisLog(false)}
                         >Sign</Button>
                     </div>
@@ -37,9 +36,9 @@ function Registration() {
                     {
                         isLog ? (
                             <div>
-                                <TextField className="outlined-basic" label="Email" variant="outlined"
+                                <TextField className="outlined-basic" label="Email*" variant="outlined"
                                            style={{marginTop: "20px"}}/>
-                                <TextField className="outlined-basic" label="Pasword" variant="outlined" type="password"
+                                <TextField className="outlined-basic" label="Password*" variant="outlined" type="password"
                                            style={{marginTop: "20px"}}/>
                                 <Button variant="contained" style={{
                                     background: "linear-gradient(to bottom,#cb42f5,#f542ec)",
@@ -51,21 +50,38 @@ function Registration() {
                             </div>
                         ) : (
                             <div>
-                                <TextField className="outlined-basic" label="FirstName" variant="outlined"
+                                <TextField className="outlined-basic" label="FirstName*" variant="outlined"
                                            style={{marginTop: "20px"}}/>
-                                <TextField className="outlined-basic" label="LastName" variant="outlined"
+                                <TextField className="outlined-basic" label="LastName*" variant="outlined"
                                            style={{marginTop: "20px"}}/>
-                                <TextField className="outlined-basic" label="Email" variant="outlined"
+                                <TextField className="outlined-basic" label="Email*" variant="outlined"
                                            style={{marginTop: "20px"}}/>
-                                <TextField className="outlined-basic" label="Pasword" variant="outlined" type="password"
+                                <TextField className="outlined-basic" label="Password*" variant="outlined" type="password"
                                            style={{marginTop: "20px"}}/>
-                                <TextField className="outlined-basic" label="Password Verify" variant="outlined"
-                                           type="password"
-                                           style={{marginTop: "20px"}}/>
-
+                                <Button
+                                    variant="contained"
+                                    component="label"
+                                    style = {{
+                                        marginTop : "10px",
+                                        width : "65%",
+                                        height: "50px"
+                                    }}
+                                >
+                                    <BiImageAdd style = {{
+                                        paddingRight : "10px",
+                                        fontSize : "20px",
+                                    }}/>
+                                    <b>
+                                    Profile Image
+                                    </b>
+                                    <input
+                                        type="file"
+                                        hidden
+                                    />
+                                </Button>
                                 <Button variant="contained" style={{
                                     background: "linear-gradient(to bottom,#cb42f5,#f542ec)",
-                                    marginTop: "20px",
+                                    marginTop: "40px",
                                     width: "70%",
                                     height: "50px"
                                 }}
@@ -73,28 +89,31 @@ function Registration() {
                             </div>
                         )
                     }
-                    <GoogleButton
-                        type="light"
-                        onClick={() => {
-                            console.log('Google button clicked')
-                        }}
-                        style={{
-                            margin: "0 auto",
+                    <div style = {{marginTop:"10px"}}>
+                        <GoogleButton
+                            type="light"
+                            onClick={() => {
+                                console.log('Google button clicked')
+                            }}
+                            style={{
+                                margin: "0 auto",
+                                width: "70%",
+                                marginTop: "10px",
+                            }}
+                        />
+                        <Button variant="contained" style={{
                             width: "70%",
-                            marginTop: "10px",
-                        }}
-                    />
-                    <Button variant="contained" style={{
-                        width: "70%",
-                        height: "50px",
-                        margin: "10px auto",
-                    }}>
-                        <SiPrivateinternetaccess style={{paddingRight: "10px"}}/>
-                        <b>Guest account</b>
-                    </Button>
-                    <div>
-                        <a href="src/Components/Auth/Registration">Forgot Password?</a>
+                            height: "50px",
+                            margin: "10px auto",
+                        }}>
+                            <SiPrivateinternetaccess style={{paddingRight: "10px"}}/>
+                            <b>Guest account</b>
+                        </Button>
+                        <div style = {{marginBottom : "20px"}}>
+                            <a href="src/Components/Auth/Registration">Forgot Password?</a>
+                        </div>
                     </div>
+
                 </form>
             </div>
         </div>
